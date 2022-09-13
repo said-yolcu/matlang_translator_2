7b6f3e3a8f51c2ab4eb07d57b8d835fef35fc938
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2022-09-13 14:44:19 +0300

    Read the file word by word

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
+
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXNAME 20
 #define MAXLINE 100
+#define NULLCHAR '\0'
+#define WORDLEN 50 // Maximum word length, including blocks
 
 typedef struct Variables
 {
@@ -15,53 +20,79 @@ typedef struct Variables
 
 typedef struct VariableNodes
 {
-    Variable *var;  // Variable of this node
-    VarNode *left;  // VarNode on the left
-    VarNode *right; // VarNode on the right. Variables on
-                    // the left has names that come earlier
-                    // on the dictionary
+    Variable *var;               // Variable of this node
+    struct VariableNodes *left;  // VarNode on the left
+    struct VariableNodes *right; // VarNode on the right. Variables on
+                                 // the left has names that come earlier
+                                 // on the dictionary
 } VarNode;
 
 VarNode root;   // Root of the variable tree
 int lineNo = 0; // Current number of line that is being read
 
-void readLines(char fileName[], char progName[])
+int main(int argc, char const *argv[])
 {
+    void readLines(const char *, const char *);
+
+    char fileName[] = "/home/said/school/spring_semester_2022/cmpe_230/homeworks/hw_1/ex1.txt";
+
+    readLines(fileName, argv[0]);
+    return 0;
+}
+
+void readLines(const char fileName[], const char progName[])
+{
+    // void processLine(const FILE *, char *);
+    char *getWord(const FILE *, char *, int);
+
     FILE *fp;
 
-    if ((fopen(fileName, "r")) == NULL)
+    if ((fp = fopen(fileName, "r")) == NULL)
     {
         fprintf(stderr, "%s: cannot open %s", progName, fileName);
         exit(2);
     }
-
-    char line[MAXLINE];
+    printf("line 53\n");
+    char word[WORDLEN];
 
     // Get each line one by one
-    while (fgets(line, MAXLINE, fp) != NULL)
+    while (getWord(fp, word, WORDLEN) != NULL)
     {
-        lineNo++;
-        processLine(line);
+        // lineNo++;
+        // printf("line 60\n");
+        // processLine(fp, line);
+        printf("%s\n", word);
+        // printf("line 62\n");
     }
+    fclose(fp);
 }
-
-void processLine(char line[])
+/*
+void processLine(const FILE *fp, char line[])
 {
+    char *getWord(char *line, char *, int);
+
     // Find the first '#' if any, and cut off this comment
-    for (int i = 0; line[i] != '\0'; i++)
+    for (int i = 0; line[i] != NULLCHAR; i++)
     {
         if (line[i] == '#')
         {
-            line[i] = '\0';
+            line[i] = NULLCHAR;
             return;
         }
     }
-}
 
-char *getWord(FILE *fp, char *word, int lim)
+    char word[WORDLEN];
+
+    while (getWord(line, word, WORDLEN) != NULL)
+    {
+        printf("%s\n", word);
+    }
+}
+*/
+char *getWord(const FILE *fp, char *word, int lim)
 {
     int c;
-    int getch(FILE *);
+    int getch(const FILE *);
     void ungetch(int);
     char *w = word;
 
@@ -72,7 +103,7 @@ char *getWord(FILE *fp, char *word, int lim)
     while (isspace(c = getch(fp)))
         ;
 
-    if (c != EOF)
+    if (c != EOF )
     {
         *w++ = c;
         if (c == '(')
@@ -87,6 +118,8 @@ char *getWord(FILE *fp, char *word, int lim)
         {
             block = 3;
         }
+    }else{
+        return NULL;
     }
 
     if (!block)
@@ -101,9 +134,10 @@ char *getWord(FILE *fp, char *word, int lim)
                 c == '=' || c == '+' || c == '-' || c == '*' || c == EOF)
             {
                 ungetch(c);
-                *w = '\0';
+                *w = NULLCHAR;
                 return word;
             }
+            *w = c;
         }
     }
     else
@@ -119,22 +153,22 @@ char *getWord(FILE *fp, char *word, int lim)
             {
                 // Raise error: unended block
             }
-            else if (c == '(' && block == 1)
+            else if (c == ')' && block == 1)
             {
                 *w++ = c;
-                *w = '\0';
+                *w = NULLCHAR;
                 return word;
             }
-            else if (c == '[' && block == 2)
+            else if (c == ']' && block == 2)
             {
                 *w++ = c;
-                *w = '\0';
+                *w = NULLCHAR;
                 return word;
             }
-            else if (c == '{' && block == 3)
+            else if (c == '}' && block == 3)
             {
                 *w++ = c;
-                *w = '\0';
+                *w = NULLCHAR;
                 return word;
             }
             else
@@ -151,17 +185,18 @@ char *getWord(FILE *fp, char *word, int lim)
 
 char buf[BUFSIZE];
 int bufP = 0;
+int lineP = 0;
 
 /*
 If there is a char in buffer, get it. Else get a char from the file.
 */
-int getch(FILE *fp)
+int getch(const FILE *fp)
 {
     if (bufP)
     {
         return buf[--bufP];
     }
-    return fgetc(fp);
+    return getc(fp);
 }
 
 /*
@@ -169,9 +204,9 @@ Unget a char to the buffer. It is used when you read too many chars by accident
 */
 void ungetch(int c)
 {
-    if (bufP >= BUFSIZE)
+    if (bufP)
     {
-        fprintf(stderr, "Cannot unget the char, buffer limit is exceeded\n");
+        fprintf(stderr, "Cannot unget the char, buffer is full\n");
         return;
     }
     buf[bufP++] = c;
